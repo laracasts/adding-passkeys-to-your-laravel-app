a318aa68c3d037e16fa4908eca030a77a25de8e7
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-06-17 11:16:17 +0100

    Episode 1 homework

--- resources/views/profile/partials/manage-passkeys.blade.php
+++ resources/views/profile/partials/manage-passkeys.blade.php
@@ -1,6 +1,6 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 id="managePasskeys" class="text-lg font-medium text-gray-900">
             {{ __('Manage Passkeys') }}
         </h2>
 
